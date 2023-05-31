013fb7a26c38a2a53a118abaaabe9cf2f46ff18b
Nikita_Vikulov <75396341+Nikita-Vikulov@users.noreply.github.com>
2023-05-31 13:55:42 +0400

    AddDaggeer

--- app/src/main/java/com/example/resotest/room/ResoDatabase.kt
+++ app/src/main/java/com/example/resotest/room/ResoDatabase.kt
@@ -1,11 +1,8 @@
 package com.example.resotest.room
 
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.resotest.DATA_BASE
 import com.example.resotest.model.Agencies
 import com.example.resotest.model.Subject
 
@@ -15,22 +12,4 @@ abstract class ResoDatabase : RoomDatabase() {
     abstract fun getAgenciesDao(): AgenciesDao
     abstract fun getSubjectDao(): SubjectDao
 
-    companion object {
-        @Volatile
-        private var INSTANCE: ResoDatabase? = null
-
-        fun getDatabase(context: Context): ResoDatabase {
-
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    ResoDatabase::class.java,
-                    DATA_BASE
-                ).build()
-                INSTANCE = instance
-
-                instance
-            }
-        }
-    }
 }
